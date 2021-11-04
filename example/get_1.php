94788fd2e468f80ed2b19ec241ad870114f92dac
a5436539 <501807312@qq.com>
2021-11-04 17:48:17 +0800

    1

---
+++ example/get_1.php
@@ -0,0 +1,86 @@
+<?php
+
+//报告E_NOTICE之外的所有错误(可解决变量不存在导致的错误，如GET、POST数据)
+error_reporting(E_ALL & ~E_NOTICE);
+
+require_once './vendor/autoload.php';
+require_once '../src/HttpClient.php';
+
+$options = [
+    'timeout'   => 10,
+    'redirects' => true,
+    'proxyIps'    => [
+        //'163.125.114.179:8088'
+    ],
+    //'cookieJarFile' => 'D:\www\test\http-client\example\cookie.txt',
+];
+$http    = new \Webguosai\HttpClient($options);
+$response = $http->get('http://www.baidu.com');
+if ($response->ok()) {
+    dump($response->body);
+} else {
+    dump($response->getErrorMsg());
+}
+
+$url = 'http://test.com/test/http-client/example/test.php?a=1';
+//$url = 'http://127.0.0.1:10453/think';
+//$url = 'http://waophp.com/api/test';
+//$url = 'https://www.qq.com/';
+//$url = 'https://www.kuaidaili.com/free/';
+
+//数组传递
+$headers = [
+    'User-Agent: my browser',//重复的
+    'Diy1: 111',//无key的解析
+    'Diy2' => '222',
+    //'Cookie: cookie=6666666',
+];
+//纯字符串(\r\n)
+$headers2 = 'Accept-Language: zh-CN,zh;q=0.9
+Cache-Control: max-age=0
+Connection: keep-alive';
+//纯字符串(\n)
+$headers3 = "Accept-Language: zh-CN,zh;q=0.9\nCache-Control: max-age=0\nConnection: keep-alive";
+
+$response = $http->get($url, ['get'=>'111'], $headers);
+
+//$response = $http->post($url, ['post'=>'111'], $headers);
+//$response = $http->post($url, '{"post":"222"}', $headers);
+//$response = $http->post($url, 'post1=111&post2=222', $headers);
+//$response = $http->setContentType('text')->post($url, '{"post":"222"}', $headers);
+
+//$response = $http->put($url, ['put' => '111'], $headers);
+//$response = $http->put($url, '{"put":"111"}', $headers);
+
+//$response = $http->delete($url, ['delete' => '111'], $headers);
+//$response = $http->delete($url, '{"delete":"111"}', $headers);
+
+//$response = $http->head($url, ['head' => '111'], $headers);
+//$response = $http->head($url, '{"head":"111"}', $headers);
+
+//$response = $http->options($url, ['options' => '111'], $headers);
+//$response = $http->options($url, '{"options":"111"}', $headers);
+
+/**
+ * form_data = multipart/form-data
+ * x-www-form-urlencoded = application/x-www-form-urlencoded
+ * json = application/json
+ * xml = application/xml
+ * raw = text/plain
+ */
+
+dump($response->ok());//http=200返回真
+dump($response->request); //请求
+dump($response->headers); //响应头
+dump($response->body); //响应body
+dump($response->httpStatus); //http状态码
+dump($response->info); //其它信息
+dump($response->getHtml()); //获取html
+dump($response->json()); //json
+dump($response->getErrorMsg()); //错误信息
+dump($response->getChatset()); //编码
+
+
+
+
+
---
+++ example/test.php
@@ -0,0 +1,56 @@
+<?php
+
+//报告E_NOTICE之外的所有错误(可解决变量不存在导致的错误，如GET、POST数据)
+error_reporting(E_ALL & ~E_NOTICE);
+
+
+/* 跳转的测试 */
+//$a = empty($_GET['a']) ? 0 : intval($_GET['a']);
+//if ($a <= 5) {
+//    $url = "?a=".($a+1);
+//    header("Location: {$url}");
+//    echo '要跳转到'.$url;
+//    exit;
+//}
+
+/** cookie设置 **/
+if (!$_COOKIE['cookie']) {
+    setcookie('cookie', 'cookie666');
+}
+
+/** http状态 **/
+//header("HTTP/1.0 404 Not Found");
+//header("HTTP/1.0 502 Bad Gateway");
+
+
+$data = [
+    'method'  => $_SERVER['REQUEST_METHOD'],
+    'headers' => getAllHeaders(),
+    'get'     => $_GET,
+    'post'    => $_POST,
+    'put'     => json_decode(file_get_contents('php://input'), true),
+    'request' => $_REQUEST,
+    'file'    => $_FILES,
+    'cookie'  => $_COOKIE,
+    'ip'      => $_SERVER['REMOTE_ADDR'],
+    'php_input' => file_get_contents('php://input'),
+    'php_input_json' => json_decode(file_get_contents('php://input'), true),
+    'server'  => $_SERVER,
+];
+
+header('Content-type: application/json; charset=utf-8');
+echo json_encode($data);
+
+
+
+//function getAllHeaders2()
+//{
+//    foreach ($_SERVER as $name => $value)
+//    {
+//        if (substr($name, 0, 5) == 'HTTP_')
+//        {
+//            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+//        }
+//    }
+//    return $headers;
+//}
