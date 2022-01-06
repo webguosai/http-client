f938847f273aff0e418572ec65f20e3a22651a2b
a5436539 <501807312@qq.com>
2022-01-06 16:41:04 +0800

    1

--- test/test.php
+++ test/test.php
@@ -1,10 +1,10 @@
 <?php
 
-//报告E_NOTICE之外的所有错误(可解决变量不存在导致的错误，如GET、POST数据)
-error_reporting(E_ALL & ~E_NOTICE);
-
 require_once '../vendor/autoload.php';
 require_once '../src/HttpClient.php';
+
+//报告E_NOTICE之外的所有错误(可解决变量不存在导致的错误，如GET、POST数据)
+error_reporting(E_ALL & ~E_NOTICE);
 
 $options = [
     'timeout'   => 10,
